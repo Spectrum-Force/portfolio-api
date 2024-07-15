23605f1d4e672e76b2b427ebd92ae0be6ca7c7ad
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-07-15 11:45:00 +0000

    Edited remoteUpload

--- routers/user_routes.js
+++ routers/user_routes.js
@@ -1,6 +1,6 @@
 import { getUser, getUsers, login, signup } from "../controllers/user_controller.js";
 import { Router } from "express";
-import { checkUserSession } from "../middlewares/auth.js";
+// import { checkUserSession } from "../middlewares/auth.js";
 
 export const userRouter = Router();
 
@@ -10,24 +10,24 @@ userRouter.post('/users/auth/login', login);
 userRouter.get('/users/auth/:userName', getUser);
 
 
-userRouter.post(
-    '/users/userProfile',
-    remoteUpload.fields([
-        {name: "profilePicture", maxCount: 1},
-        {name: "resume", maxCount: 1},
-    ]),
-    checkUserSession,
-    createUserProfile
-);
+// userRouter.post(
+//     '/users/userProfile',
+//     remoteUpload.fields([
+//         {name: "profilePicture", maxCount: 1},
+//         {name: "resume", maxCount: 1},
+//     ]),
+//     checkUserSession,
+//     createUserProfile
+// );
 
-userRouter.patch(
-    '/users/userProfile/:id',
-    remoteUpload.fields([
-        {name: "profilePicture", maxCount: 1},
-        {name: "resume", maxCount: 1},
-    ]),
-    checkUserSession, 
-    updateUserProfile
-);
+// userRouter.patch(
+//     '/users/userProfile/:id',
+//     remoteUpload.fields([
+//         {name: "profilePicture", maxCount: 1},
+//         {name: "resume", maxCount: 1},
+//     ]),
+//     checkUserSession, 
+//     updateUserProfile
+// );
 
 
